53da7aade4f219625c67d1b75d03c986bd27aff3
GaryLamKa <39815026+GaryLamKa@users.noreply.github.com>
2019-04-24 13:49:53 +1000

    Update footer.php

--- Aramat/footer.php
+++ Aramat/footer.php
@@ -1,5 +1,24 @@
 <?php wp_footer();?>
 
 
+<div class="footer-container" style="background-color: gray; height:100%; width:100%;">
+        <div class="ft-social" style="text-align: center; padding-top: 20px;">
+                <a href="#" class="support" style="color:black;">Contact Us</a>
+                <br>
+                <a href="#" class="face" style="color:black;">Facebook</a>
+                <br>
+                <a href="#" class="tweet" style="color:black;">Twitter</a>
+                <br>
+                <a href="#" class="linked" style="color:black;">Instagram</a>
+        </div>
+
+        <br>
+        <br>
+        <br>
+
+        <div class="copyright">
+                <p>&copy 2019 - ARAMAT</p>
+        </div>
+</div>
 </body>
 </html>
